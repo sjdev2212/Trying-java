1eae18cdb8882221dee655e1275bf8acc091bf79
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-07-20 15:36:59 -0300

    crear clase coche y funcion para agregar puerta

--- Main.java
+++ Main.java
@@ -1,13 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        Coche miCoche = new Coche();
+        miCoche.agregarPuerta();
         int resultado = 0;
         resultado = sumeTres(5, 15, 15);
-        System.out.println(resultado);
+        System.out.println(miCoche.puertas);
     }
     
     public static int sumeTres(int a, int b, int c) {
         return a + b + c;
     }
+
         
 }
+class Coche {
+    int puertas = 2;
 
+  public void agregarPuerta () {
+    this.puertas++;
+  }
+
+}
