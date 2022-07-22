fc0cfceace73ba9b31129a35d73424f57d1a7e37
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-07-22 20:33:07 -0300

    resultados ejercicio 8

--- Main.java
+++ Main.java
@@ -1,18 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Coche miCoche = new Coche();
-        miCoche.agregarPuerta();
-        int resultado = 0;
-        resultado = sumeTres(5, 15, 15);
-        System.out.println(miCoche.puertas);
-    }
-    
-    public static int sumeTres(int a, int b, int c) {
-        return a + b + c;
-    }
 
-        
+
+        Persona anibal = new Persona();
+        anibal.setNombre("Anibal");
+        anibal.setEdad(44);
+        anibal.setTelefono(11256565);
+
+        System.out.println(anibal.getNombre());
+        System.out.println(anibal.getEdad());
+        System.out.println(anibal.getTelefono());
+
 }
+ }
+
+
+
+    //////////////////////////////////////////////////////////////////
+
+     
 class Coche {
     int puertas = 2;
 
@@ -21,3 +27,32 @@ class Coche {
   }
 
 }
+class Persona {
+private int edad;
+private String nombre;
+private int telefono;
+
+
+public  void setEdad (int edad) {
+    this.edad = edad;
+}
+public  void setNombre (String nombre) {
+    this.nombre = nombre;
+
+}
+public  void setTelefono (int telefono) {
+    this.telefono = telefono;
+
+ }
+ public  int getEdad () {
+    return this.edad;  
+    }  
+  public  String getNombre () {
+    return this.nombre;
+
+
+     }
+  public  int getTelefono () {
+    return this.telefono;
+     }
+      }
